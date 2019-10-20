3941637fad177f2408e0abd98d962ef5c12b7fe3
sandeepini <sandeepkondaiah@gmail.com>
2019-10-20 11:25:20 +0530

    modify user test case

---
+++ SampleMavenProject/src/test/java/com/actitime/test/Modifyuser.java
@@ -0,0 +1,11 @@
+package com.actitime.test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Modifyuser {
+	@Test
+	public void testModifyuser() {
+		Reporter.log("testModifyuser",true);
+	}
+}
