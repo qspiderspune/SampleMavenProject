1e32aa802f6cc3d60a53dfb063bc47ab1e898f15
sandeepini <sandeepkondaiah@gmail.com>
2019-10-20 11:17:07 +0530

    First commit-export

---
+++ SampleMavenProject/src/test/java/com/actitime/test/CreateUser.java
@@ -0,0 +1,20 @@
+package com.actitime.test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateUser {
+	
+	@Test
+	public void testCreateUser() {
+		Reporter.log("testCreateUser",true);
+	}
+	@Test
+	public void testCreateProject() {
+		Reporter.log("testCreateproject",true);
+	}
+	@Test
+	public void testVerifyBuild() {
+		Reporter.log("testVerifyBuild",true);
+	}
+}
